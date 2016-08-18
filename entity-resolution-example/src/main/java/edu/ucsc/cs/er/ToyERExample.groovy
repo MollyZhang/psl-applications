4410b80598e790f0fc7f0376e2df35ff668ff3d0
Molly Zhang <mollyzhang@ucsc.edu>
2016-08-18 14:07:57 -0700

    add psl.properties for entity resolution example and setup the boiler plate of PSL model

--- entity-resolution-example/src/main/java/edu/ucsc/cs/er/ToyERExample.groovy
+++ entity-resolution-example/src/main/java/edu/ucsc/cs/er/ToyERExample.groovy
@@ -1,1 +1,22 @@
 package edu.ucsc.cs.er
+
+import edu.umd.cs.psl.config.*
+import edu.umd.cs.psl.database.DataStore
+import edu.umd.cs.psl.database.rdbms.RDBMSDataStore 
+import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver
+import edu.umd.cs.psl.database.rdbms.driver.H2DatabaseDriver.Type
+import edu.umd.cs.psl.groovy.PSLModel
+
+
+//////////////////////// initial setup //////////////////////////
+ConfigManager cm = ConfigManager.getManager();
+ConfigBundle config = cm.getBundle("er");
+
+def defaultPath = System.getProperty("java.io.tmpdir")
+String dbpath = config.getString("dbpath", defaultPath + File.separator + "entity-resolution-example")
+DataStore data = new RDBMSDataStore(new H2DatabaseDriver(Type.Disk, dbpath, true), config)
+PSLModel m = new PSLModel(this, data)
+
+
+println "hey there"
+println this
